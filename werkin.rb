11bdc9ee76fed444ecf80d56d059cf90c52b4aee
jjhiggz <jonathan.higger@gmail.com>
2021-02-08 15:20:28 -0700

    second commit

---
+++ werkin.rb
@@ -0,0 +1,41 @@
+def people
+    [
+        {
+            name: "Chris",
+            age: 21
+        },
+        {
+            name: "Blake",
+            age: 22
+        },
+        {
+            name: "Chris",
+            age: 21
+        },
+        {
+            name: "Chris",
+            age: 21
+        },
+        {
+            name: "Chris",
+            age: 21
+        },
+        {
+            name: "Chris",
+            age: 21
+        },
+        {
+            name: "Chris",
+            age: 21
+        },
+        {
+            name: "Chris",
+            age: 21
+        },
+        {
+            name: "Chris",
+            age: 21
+        },
+    ]
+end
+
